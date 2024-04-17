6a2cce0843c2784db611748e8aa851a8f95afdc7
veruzhskaya <veruzhskaya@gmail.com>
2024-04-17 12:51:37 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./app.css"
+import FSD from "./FSD.xlsx"
+
+function App() {
+  return (
+    <div className="App">
+        <button> <a href={FSD}>Скачать файл</a></button>
+    </div>
+  );
+}
+
+export default App;
